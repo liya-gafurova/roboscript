3b065e705c09a138fd6d65a0fd239cae29fd4210
lgafurova <lgafurova@issart.com>
2020-01-30 18:18:44 +0600

    add argparse

--- lib.py
+++ lib.py
@@ -1,24 +1,44 @@
 import csv
 import datetime
 import json
-import  xml.dom.minidom # можно конечно использовать lxml, но мб лучше избежать лишних импортов
+import os
+import time
+import  xml.dom.minidom
+import requests
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 
-
-download_path = r'/home/lgafurova/Downloads/'
-export_file_name = 'export_survey_37241.csv'
-orders_filename = '5orders.json'
 roboforex_accounts_filename = 'accounts.xml'
-subscribe_period = 30 # количество дней , на которое распространяется подписка
-default_lowest_date = '1900-01-01 00:00:00'
-params = {'status' : 'Завершен', 'Title': 'Инвест-Клуб', 'subscribe_period' : subscribe_period }
 
-file_path =download_path + export_file_name
+# course_name = 'forinvestcourse' # TODO название курса , robovladelets
+# secret_key = 'FysVeUnB6lOE0k9SU01lNFDGoAY8gR8GsklKsQjRzEVEGR4Rfg7T7tBjkyljQSKZxQHhpDLeb7U2y5eONrvNuEbaXFvAKK8QHj0yKRySAN193GhrqSZElpr9t7A6LEfG' # TODO get_course API key
+# subscribe_period = 30 # TODO количество дней , на которое распространяется подписка
+# USERNAME = 'gali4086555@gmail.com' # TODO username на get_course, админка
+# PASSWORD = 'f54c045e' # TODO пароль на get_course, админка
+# DATAURL = 'https://{}.getcourse.ru/user/control/surveyAnswer/list/surveyId/37241'.format(course_name) # TODO ulr до анкеты
+# download_path = r'/home/lgafurova/Downloads/'  # TODO путь до Загрузок
 
+def get_export_id(account_name:str, secret_key: str,params : str):
+    str_request = 'https://{0}.getcourse.ru/pl/api/account/users?key={1}&{2}'.format(account_name, secret_key, params)
+    res_json = requests.get(str_request).json()
+    return res_json['info']['export_id']
 
-needed_fields_survey = ['Email','Номер счета']
+def get_client_json_list(account_name : str , secret_key : str, export_id: str ):
+    str_request = 'https://{0}.getcourse.ru/pl/api/account/exports/{1}?key={2}'.format(account_name, export_id, secret_key)
+    return requests.get(str_request).json()
 
+def get_orders_export_id(account_name, secret_key, status):
+    str_req = 'https://{0}.getcourse.ru/pl/api/account/deals?key={1}&status={2}'.format(account_name,secret_key, status)
+    json = requests.get(str_req).json()
+    try:
+        return json['info']['export_id']
+    except Exception as e:
+        print('Exception = {}'.format(e))
 
-needed_fields_order = ['Номер', 'Email', 'Дата оплаты','Title',  'Статус']
+def get_orders_list (account_name : str , secret_key : str, export_id: str ):
+    str_request = 'https://{}.getcourse.ru/pl/api/account/exports/{}?key={}'.format(account_name, export_id,secret_key)
+    return requests.get(str_request).json()
+
 
 def coalesce(*arg):
   for el in arg:
@@ -44,32 +64,30 @@ def get_data_from_exported_survey(file_path, needed_fields):
         for row in survey_reader:
             accounts = []
             if iterrows == 0:
-                print('columns = '+ str(row))
                 iterrows +=1
                 indesies = find_indesies_of_fields(row, needed_fields)
-                print('')
             else:
-                print('rows = ' + str(row))
                 for index in indesies:
                     accounts.append(row[index])
                 iterrows += 1
                 accounts_all.append(accounts)
-        print('survey  = ' + str(accounts_all))
+        #print('survey  = ' + str(accounts_all))
     return accounts_all
 
-
-def get_data_from_exported_orders(file_name, needed_order_fields):
-    #  передаем файл (JSON) и список полей, которые нужно вытащить
-    orders_all = []
+def get_json(file_name):
     with open(file_name) as orders_json:
         data = ''.join(orders_json.readlines())
     json_string= json.loads(data)
+    return json_string
+
+def get_data_from_exported_orders(json_string, needed_order_fields):
+    #  передаем файл (JSON) и список полей, которые нужно вытащить
+    orders_all = []
 
     # получаем индексы полей, где будут содержаться необходимые данные
     orders_indesies = find_indesies_of_fields(json_string['info']['fields'], needed_order_fields)
 
     for order in json_string['info']['items']:
-        # print('str = '+str(order))
         order_row = []
         # TODO сделать не списолк списков, а список кортежей
         for orders_index  in orders_indesies:
@@ -91,9 +109,10 @@ def filter_orders(orders : list, params:dict):
     earliest_payment_day = current_date - datetime.timedelta(days=params['subscribe_period'])
     filtered_orders = []
     for order in orders:
-        order[2] =   order[2] if order[2]!= '' else default_lowest_date
-        payment_date = coalesce(datetime.datetime.strptime(order[2], "%Y-%m-%d %H:%M:%S"))
-        if order[4] == params['status'] and order[3] == params['Title'] and payment_date >= earliest_payment_day:
+         # TODO убрать обращение по индексу
+        order[1] =   order[1] if order[1]!= '' else default_lowest_date
+        payment_date = coalesce(datetime.datetime.strptime(order[1], "%Y-%m-%d %H:%M:%S"))
+        if order[3] == params['status'] and order[2] == params['Title'] and payment_date >= earliest_payment_day:
             filtered_orders.append(order)
     print('filtered_orders' + str(filtered_orders))
     return  filtered_orders
@@ -105,25 +124,46 @@ def group_survey_answers(raw_surveys: list):
     survey_dict = dict.fromkeys(keys, [])
     for survey in raw_surveys:
         survey_dict[survey[0]].append(survey[1])
-    print(survey_dict)
+    print('surveys = ' + str(survey_dict))
     return  survey_dict
 
+def get_elem_by_name(elem_list, name):
+    for e in elem_list:
+        if name in e.text:
+            return  e
 
 
-#получаем данные-анкеты
-surveys = group_survey_answers(get_data_from_exported_survey(file_path, needed_fields_survey))
-# получаем данные - заказы
-orders = filter_orders(get_data_from_exported_orders(orders_filename , needed_fields_order), params = params)
+def get_csv_surveys(username , password, dataurl,course_name , driver_path):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(driver_path)  # , options= chrome_options
+    driver.get('https://{}.getcourse.ru/cms/system/login'.format(course_name))
 
-# получаем данные - счета робофорекса
-accounts_roboforex = get_data_from_exported_accounts(roboforex_accounts_filename)
+    # Ввод логина
+    login = driver.find_element_by_name("email")
+    login.clear()
+    login.send_keys(username)
 
-result = {}
-for appropriate_order in orders:
-    if appropriate_order[1] in surveys.keys():
-        accounts = set(surveys[appropriate_order[1]])
-        account_intersection = accounts.intersection(accounts_roboforex)
-        result[appropriate_order[1]] = account_intersection
+    # Ввод пароля
+    pswd = driver.find_element_by_name("password")
+    pswd.send_keys(password)
 
-print('RESULT = {}'.format(result))
+    # Жмем войти
+    # 1 - ищем кнопку по названию, id формируется динамически. сначала ищем все кнопки, из них выбираем по названию
+    elem = driver.find_elements_by_tag_name('button')
+    el = get_elem_by_name(elem, 'Войти').click()
+    time.sleep(2)  # Ждет загрузки браузера
+
+    driver.get(dataurl)  # переходит на страницу с экспортом csv, но не экспортирует
+    # экспортирует, сохраняет в загрузки
+    survey_page_elem = driver.find_element_by_link_text('Экспорт (CSV)').click()
+    time.sleep(5)  # ждем скачки файла
+
+
+    csv_file_with_survey = group_survey_answers(get_data_from_exported_survey(file_path, needed_fields_survey))
+
+    os.remove(file_path)
+
+    driver.quit()
+    return csv_file_with_survey
 
--- script.py
+++ script.py
@@ -1,53 +1,66 @@
-import requests
 import time
+from lib import *
+import argparse
 
-# документация к getcourse API
-# https://getcourse.ru/blog/275796?gcmes=3912939093&gcmlg=31873
-# https://getcourse.ru/blog/275993
-# https://getcourse.ru/blog?tagNames=%D1%8D%D0%BA%D1%81%D0%BF%D0%BE%D1%80%D1%82&archived=actual
-# https://getcourse.ru/help/api#users
 
-# сгенерировать секретный ключ https://forinvestcourse.getcourse.ru/saas/account/api
-# get запрос на эспорт клиентов
-# get запрос на получение данных о клиентах
-course_name = 'forinvestcourse'
-secret_key = 'FysVeUnB6lOE0k9SU01lNFDGoAY8gR8GsklKsQjRzEVEGR4Rfg7T7tBjkyljQSKZxQHhpDLeb7U2y5eONrvNuEbaXFvAKK8QHj0yKRySAN193GhrqSZElpr9t7A6LEfG'
+default_lowest_date = '1900-01-01 00:00:00'
+params = {'status' : 'Завершен', 'Title': 'Инвест-Клуб', 'subscribe_period' : subscribe_period }
+needed_fields_survey = ['Email','Номер счета'] # TODO в зависимосмости от того, как у заказчика сформирована анкета -- вопрос к заказчику
+needed_fields_order = ['Email', 'Дата оплаты','Title',  'Статус']
 
 
 
-def get_export_id(account_name:str, secret_key: str,params : str):
-    str_request = 'https://{0}.getcourse.ru/pl/api/account/users?key={1}&{2}'.format(account_name, secret_key, params)
-    res_json = requests.get(str_request).json()
-    print('export_id = {}'.format(res_json['info']['export_id']))
-    return res_json['info']['export_id']
 
-def get_client_json_list(account_name : str , secret_key : str, export_id: str ):
-    str_request = 'https://{0}.getcourse.ru/pl/api/account/exports/{1}?key={2}'.format(account_name, export_id, secret_key)
-    return requests.get(str_request).json()
 
-def get_orders_export_id(account_name, secret_key, params):
-    str_req = 'https://{0}.getcourse.ru/pl/api/account/deals?key={1}&status={2}'.format(account_name,secret_key, 'new')
-    json = requests.get(str_req).json()
-    print('order_export_id = '+str(json['info']['export_id']))
-    return  json['info']['export_id']
+def createParser():
+    parser = argparse.ArgumentParser()
+    # создаем аргументы, которые ожидаем передавать при запуске скрипта
+    parser.add_argument('--course_name', default='forinvestcourse')
+    parser.add_argument('--get_course_api_key', default='FysVeUnB6lOE0k9SU01lNFDGoAY8gR8GsklKsQjRzEVEGR4Rfg7T7tBjkyljQSKZxQHhpDLeb7U2y5eONrvNuEbaXFvAKK8QHj0yKRySAN193GhrqSZElpr9t7A6LEfG')
+    parser.add_argument('--subscribe_period', default=30)
+    parser.add_argument('--get_course_username', default='gali4086555@gmail.com')
+    parser.add_argument('--get_course_password', default='f54c045e')
+    parser.add_argument('--survey_url', default='https://forinvestcourse.getcourse.ru/user/control/surveyAnswer/list/surveyId/37241')
+    parser.add_argument('--downloads_path', default= r'/home/lgafurova/Downloads/')
 
-def get_orders_list (account_name : str , secret_key : str, export_id: str ):
-    str_request = 'https://{}.getcourse.ru/pl/api/account/exports/{}?key={}'.format(account_name, export_id,secret_key)
-    print(requests.get(str_request).json())
-    return requests.get(str_request).json()
 
-# TODO опредилить статус пользователя, имеющего оплаченную подписку
-export_id = get_export_id(course_name, secret_key, 'status=active' )
-# останавливаем скрипт для того, чтобы на сайте успели собраться данные.
-# Если второй запрос отправлять без паузы, данные не успевают собраться 'error_message': 'Файл еще не создан '
-time.sleep(10)
+    return  parser
 
-exported_clients = get_client_json_list(course_name, secret_key,export_id)
-print(exported_clients)
+if __name__ == "__main__":
+    parser= createParser()
+    namespase = parser.parse_args()
+    LOGINURL = 'https://{}.getcourse.ru/cms/system/login'.format(namespase.course_name)
+    driver_path = './chromedriver_linux64/chromedriver'
+    survey_id = namespase.survey_url.split('/')[-1]
+    export_file_name = 'export_survey_{}.csv'.format(survey_id)
+    file_path = namespase.downloads_path + export_file_name
 
-# экспорт заказов
-status = 'payed'
-params = {'status':status}
-order_export_id = get_orders_export_id(course_name, secret_key, params)
-time.sleep(10)
-get_orders_list(course_name, secret_key,order_export_id)
+
+    # 1. Сбор данных
+    # 1.1 экспорт заказов из get_course
+    status = 'new'  # TODO 'payed' заказ в статусе "Завершен"
+    order_export_id = get_orders_export_id(course_name, secret_key, status)
+    # останавливаем скрипт для того, чтобы на сайте успели собраться данные.
+    # Если второй запрос отправлять без паузы, данные не успевают собраться 'error_message': 'Файл еще не создан '
+    time.sleep(10)
+    orders_list_json = get_orders_list(course_name, secret_key, order_export_id)
+    orders = filter_orders(get_data_from_exported_orders(orders_list_json, needed_fields_order), params=params)
+
+    # 1.2 Сбор анкет csv
+    surveys = get_csv_surveys(namespase.get_course_username, namespase.get_course_password, namespase.survey_url , namespase.course_name , driver_path)
+
+    # 1.3 сбор данных с робофоркес
+    accounts_roboforex = get_data_from_exported_accounts(
+        roboforex_accounts_filename)  # TODO сделать получение из апишки
+
+    # 2 Логика обработки
+    result = {}
+    for appropriate_order in orders:
+        if appropriate_order[1] in surveys.keys():
+            accounts = set(surveys[appropriate_order[1]])
+            account_intersection = accounts.intersection(accounts_roboforex)
+            result[appropriate_order[1]] = account_intersection
+
+    print('e-mails with approved accounts = {}'.format(result))
+
+    print('')
